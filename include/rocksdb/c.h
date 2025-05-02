1d33759da562ba183bdb1164d443cceda301b5e7
Pavel Tcholakov <pavel@restate.dev>
2025-05-02 09:42:55 +0200

    Add FFI TablePropertiesCollector support
    This change introduces FFI functions for registering TablePropertiesCollector
    factory and implementing the necessary callbacks.

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -86,6 +86,13 @@ typedef struct rocksdb_compactionfiltercontext_t
     rocksdb_compactionfiltercontext_t;
 typedef struct rocksdb_compactionfilterfactory_t
     rocksdb_compactionfilterfactory_t;
+typedef struct rocksdb_table_properties_t rocksdb_table_properties_t;
+typedef struct rocksdb_user_collected_properties_t
+    rocksdb_user_collected_properties_t;
+typedef struct rocksdb_table_properties_collector_t
+    rocksdb_table_properties_collector_t;
+typedef struct rocksdb_table_properties_collector_factory_context_t
+    rocksdb_table_properties_collector_context_t;
 typedef struct rocksdb_comparator_t rocksdb_comparator_t;
 typedef struct rocksdb_dbpath_t rocksdb_dbpath_t;
 typedef struct rocksdb_env_t rocksdb_env_t;
@@ -1191,6 +1198,8 @@ extern ROCKSDB_LIBRARY_API void rocksdb_reset_status(
     rocksdb_status_ptr_t* status_ptr);
 extern ROCKSDB_LIBRARY_API uint32_t
 rocksdb_flushjobinfo_flush_reason(const rocksdb_flushjobinfo_t* info);
+extern ROCKSDB_LIBRARY_API const rocksdb_table_properties_t*
+rocksdb_flushjobinfo_table_properties(const rocksdb_flushjobinfo_t*);
 
 /* Compaction job info */
 extern ROCKSDB_LIBRARY_API void rocksdb_compactionjobinfo_status(
@@ -1933,6 +1942,50 @@ extern ROCKSDB_LIBRARY_API unsigned char rocksdb_options_get_atomic_flush(
 
 extern ROCKSDB_LIBRARY_API void rocksdb_options_set_row_cache(
     rocksdb_options_t* opt, rocksdb_cache_t* cache);
+
+extern ROCKSDB_LIBRARY_API void
+rocksdb_options_add_table_properties_collector_factory(
+    rocksdb_options_t* options, void* state, void (*destructor)(void* state),
+    const char* (*name)(void*),
+    rocksdb_table_properties_collector_t* (*create_collector)(
+        void* state, rocksdb_table_properties_collector_context_t* context));
+
+extern ROCKSDB_LIBRARY_API rocksdb_table_properties_collector_t*
+rocksdb_table_properties_collector_create(
+    void* state, void (*destructor)(void* state),
+    bool (*add_user_key)(void*, const char* key, size_t key_len,
+                         const char* value, size_t value_len, int entry_type,
+                         uint64_t sequence_number, uint64_t file_size),
+    void (*block_add)(void*, uint64_t, uint64_t, uint64_t),
+    bool (*finish)(void*, rocksdb_user_collected_properties_t* properties),
+    void (*get_readable_properties)(
+        void*, rocksdb_user_collected_properties_t* properties),
+    const char* (*name)(void*));
+
+extern ROCKSDB_LIBRARY_API uint32_t
+rocksdb_table_properties_collector_context_get_column_family_id(
+    rocksdb_table_properties_collector_context_t*);
+
+extern ROCKSDB_LIBRARY_API int
+rocksdb_table_properties_collector_context_get_level_at_creation(
+    rocksdb_table_properties_collector_context_t*);
+
+extern ROCKSDB_LIBRARY_API int
+rocksdb_table_properties_collector_context_get_num_levels(
+    rocksdb_table_properties_collector_context_t*);
+
+extern ROCKSDB_LIBRARY_API uint64_t
+rocksdb_table_properties_collector_context_get_last_level_inclusive_max_seqno_threshold(
+    rocksdb_table_properties_collector_context_t*);
+
+extern ROCKSDB_LIBRARY_API const char*
+rocksdb_table_properties_get_user_collected_property(
+    const rocksdb_table_properties_t* table_properties, const char* key);
+extern ROCKSDB_LIBRARY_API void rocksdb_table_properties_destroy(
+    const rocksdb_table_properties_t*);
+
+extern ROCKSDB_LIBRARY_API void rocksdb_user_collected_properties_insert(
+    rocksdb_user_collected_properties_t*, const char*, const char*);
 
 extern ROCKSDB_LIBRARY_API void
 rocksdb_options_add_compact_on_deletion_collector_factory(
